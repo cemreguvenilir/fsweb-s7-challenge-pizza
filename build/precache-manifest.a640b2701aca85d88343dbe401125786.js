dc35b0128d3c868caf9b15610b2164f71a0e63d6
cemreguvenilir <guvenilircemre@gmail.com>
2023-07-04 17:13:16 +0300

    deneme2

---
+++ build/precache-manifest.a640b2701aca85d88343dbe401125786.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "81dcd8c8386c2bb1d020",
+    "url": "/static/css/main.4ae89bb8.chunk.css"
+  },
+  {
+    "revision": "81dcd8c8386c2bb1d020",
+    "url": "/static/js/main.81dcd8c8.chunk.js"
+  },
+  {
+    "revision": "e675664438b1ad850f4f",
+    "url": "/static/js/1.e6756644.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "956183d81336869ac03c8f3e3808fbef",
+    "url": "/index.html"
+  }
+];
